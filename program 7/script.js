742f30ff60cc23daa1eabbf6801decd29ac50d95
poornachandraTejaswiP <142889226+poornachandraTejaswiP@users.noreply.github.com>
2024-12-22 06:10:55 +0530

    Add files via upload

---
+++ program 7/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#learn-more").on("click", function () {
+        alert("Thank you for visiting! Explore my Resume and Bio-data for more information.");
+    });
+});
